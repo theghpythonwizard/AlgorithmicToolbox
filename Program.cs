1f040407151c376f4c5eef1fa665ff93beb5e9b6
Jason Schultheis <jasschul@microsoft.com>
2022-08-13 19:15:58 +0000

    hate copespaces sometimes. stop freezing on me

--- Program.cs
+++ Program.cs
@@ -6,13 +6,19 @@ namespace Addition
     {
         static void Main(string[] args)
         {
-            Console.Write("input first value: ");
-            var varA = Console.ReadLine();
-            Console.Write("input second value: ");
-            var varB = Console.ReadLine();
-            var intA = int.Parse(varA);
-            var intB = int.Parse(varB);
-            Console.WriteLine(intA + intB);
+            // Console.Write("input first value: ");
+            // var varA = Console.ReadLine();
+            // Console.Write("input second value: ");
+            // var varB = Console.ReadLine();
+            // var intA = int.Parse(varA);
+            // var intB = int.Parse(varB);
+            // Console.WriteLine(intA + intB);
+            int[] array1 = new int[5];
+            foreach (int n in array1)
+            {
+                Console.WriteLine(n);
+            }
+            StressTest.Run();
         }
     }
 }
