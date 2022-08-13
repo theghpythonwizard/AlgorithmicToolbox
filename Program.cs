ed4fd67e9d54be8a99c62e5e05ea4450f141ca34
Jason Schultheis <jasschul@microsoft.com>
2022-08-13 20:13:16 +0000

    adding to one file

--- Program.cs
+++ Program.cs
@@ -4,6 +4,38 @@ namespace Addition
 {
     class Program
     {
+        private static int MaxPairwiseProduct(int[] numbers)
+        {
+            int result = 0;
+            int n = numbers.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                for (int j = i + 1; j < n; ++j)
+                {
+                    if (numbers[i] * numbers[j] > result)
+                    {
+                        result = numbers[i] * numbers[j];
+                    }
+                }
+            }
+            return result;
+        }
+        
+        private static void RunStressTest()
+        {
+            int n;
+            var tmp = Console.ReadLine();
+            n = int.Parse(tmp);
+            int[] numbers = new int[n];
+            string[] arrayNumbers = Console.ReadLine().Split();
+            for (int i = 0; i < n; ++i)
+            {
+                numbers[i] = int.Parse(arrayNumbers[i].ToString());
+            }
+
+            int result = MaxPairwiseProduct(numbers);
+            Console.WriteLine(result);
+        }
         static void Main(string[] args)
         {
             // Console.Write("input first value: ");
@@ -23,7 +55,7 @@ namespace Addition
             // {
             //     Console.WriteLine(n);
             // }
-            StressTest.Run();
+            RunStressTest();
         }
     }
 }
