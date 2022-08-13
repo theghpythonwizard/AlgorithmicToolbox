d5af8ab23742616710646313a9040712450a782c
Jason Schultheis <jasschul@microsoft.com>
2022-08-13 18:14:19 +0000

    pushing initial file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace APlusB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+            // var tokens = input.Split(' ');
+            // var a = int.Parse(tokens[0]);
+            // var b = int.Parse(tokens[1]);
+            // Console.WriteLine(a + b);
+        }
+    }
+}
